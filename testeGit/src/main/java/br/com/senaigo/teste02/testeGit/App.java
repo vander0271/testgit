d543e04447bc4c23b87c42a0e9780035f7129a04
aluno <aluno@LAB104-XX.nox5.lan>
2019-03-14 21:37:39 -0300

    Commits

---
+++ testeGit/src/main/java/br/com/senaigo/teste02/testeGit/App.java
@@ -0,0 +1,13 @@
+package br.com.senaigo.teste02.testeGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
